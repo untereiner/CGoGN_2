c258a3042eec153ef0debd20c6910411e0f62a88
Étienne Schmitt <etienne.schmitt@inria.fr>
2016-11-25 18:00:48 +0100

    fixes
    Signed-off-by: Étienne Schmitt <etienne.schmitt@inria.fr>

--- cmake/utilities.cmake
+++ cmake/utilities.cmake
@@ -92,8 +92,6 @@ export(TARGETS ${PROJECT_NAME}
 	FILE "${CMAKE_BINARY_DIR}/lib/cmake/${PROJECT_NAME}/${PROJECT_NAME}Targets.cmake"
 )
 
-#message(${package_root_dir})
-
 configure_package_config_file(
 #	"${CGOGN_PATH}/cmake/ConfigFiles/${PROJECT_NAME}Config.cmake.in"
 	"${package_root_dir}/${PROJECT_NAME}Config.cmake.in"
